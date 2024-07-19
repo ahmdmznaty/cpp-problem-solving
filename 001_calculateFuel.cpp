e3cd4b3bb9664c5145144aee81e47360b0883aa8
Ahmed M. Znaty <ahmdmznaty@gmail.com>
2024-07-19 11:44:33 +0300

    Prob_001_calculateFuel

---
+++ 001_calculateFuel.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+double calculateFuel(double distance);
+
+int main() {
+    double dis_1 = 15;
+    double dis_2 = 23.5;
+    double dis_3 = 3;
+
+    cout << "Fuel_1: " << calculateFuel(dis_1) << endl
+         << "Fuel_2: " << calculateFuel(dis_2) << endl
+         << "Fuel_3: " << calculateFuel(dis_3) << endl;
+
+    return 0;
+}
+
+double calculateFuel(double distance) {
+    double minFuel = distance * 10;
+    if(minFuel >= 100) {
+        return minFuel;
+    }
+    return 100;
+}
