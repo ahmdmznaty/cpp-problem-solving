c3863ed4d91d2afbacc981098a7ff514da9ace32
Ahmed M. Znaty <ahmdmznaty@gmail.com>
2024-08-07 12:44:04 +0300

    Prob_004_triangle

---
+++ 004_triangle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int triangle(int index) {
+    int value = 0;
+    for(int ind = 0; ind <= index; ind++) {
+        value = value + ind;
+    }
+    return value;
+}
+
+int main() {
+    cout << triangle(1) << endl // 1
+         << triangle(6) << endl // 21
+         << triangle(215) << endl; // 23220
+    return 0;
+}
+
+
+
+// Problem URL:
+// https://edabit.com/challenge/M7Xzr3ez5xBPTu3bq
